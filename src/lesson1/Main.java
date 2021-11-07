80a8e4ed75c418c0d9064d4132866f0028aa1db9
Hayk323 <93340522+Hayk323@users.noreply.github.com>
2021-11-07 16:32:33 +0400

    changed IfExample ,add IntExample

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("hello ");
         System.out.print("world");
+        for (int i = 0; i < ; i++) {
+            
+        }
     }
 }
